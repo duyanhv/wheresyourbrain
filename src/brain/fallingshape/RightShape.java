7192d374aa390e95e01816b467dab7e048723968
Vu Duy Anh <duyanh@MBP-cua-Vu.local>
2017-11-01 19:26:38 +0700

    heyhey

--- src/brain/fallingshape/RightShape.java
+++ src/brain/fallingshape/RightShape.java
@@ -1,7 +1,20 @@
 package brain.fallingshape;
 
+import bases.GameObject;
+import bases.physics.BoxCollider;
+import bases.physics.PhysicsBody;
+
 /**
  * Created by duyanh on 11/1/17.
  */
-public class RightShape {
+public class RightShape extends GameObject implements PhysicsBody {
+    @Override
+    public BoxCollider getBoxCollider() {
+        return null;
+    }
+
+    @Override
+    public void setIsActive(boolean isActive) {
+
+    }
 }
--- src/brain/fallingshape/RightSpawner.java
+++ src/brain/fallingshape/RightSpawner.java
@@ -1,7 +1,25 @@
 package brain.fallingshape;
 
+import bases.FrameCounter;
+import bases.GameObject;
+
+import java.util.Random;
+
 /**
  * Created by duyanh on 11/1/17.
  */
-public class RightSpawner {
+public class RightSpawner extends GameObject {
+
+    FrameCounter frameCounter = new FrameCounter(120);
+
+    Random rdm = new Random();
+
+    @Override
+    public void run() {
+        super.run();
+    }
+
+    private void spawn(){
+
+    }
 }
